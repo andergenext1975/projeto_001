80b4c2241fb97510361b8945ddbf59583019557c
andergenext1975 <166748642+andergenext1975@users.noreply.github.com>
2024-04-17 08:48:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ list(df)
 
 paises = list(df['area'].unique())
 variantes = list(df['variant_name'].unique())
+periodos = list(df['date'].unique())
+
 df['date'] = pd.to_datetime(df['date'],format='%Y-%m-%d')
 
 
 pais = st.sidebar.selectbox('Escolha o pais',['Todos'] + paises)
 variante = st.sidebar.selectbox('Escolha a variante',['Todas'] + variantes)
+periodo = st.sidebar.selectbox('Escolha a Data',['Todas'] + periodos)
+
+
+if(periodo != 'Todas') :
+    st.header('Mostrando resultado de ' + periodo)
+    df = df[df['date']==periodo]
+else:
+    st.header('Mostrando resultados de todo período')
 
 if(pais != 'Todos') :
     st.header('Mostrando resultado de ' + pais)
