f121eb17b2459cd5022290b67b41b9c6a00f46c9
andergenext1975 <166748642+andergenext1975@users.noreply.github.com>
2024-04-19 08:49:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -55,5 +55,35 @@ fig.update_layout(title='Casos diarios de Covid-19 by "Anderson Nascimento"')
 # Publicando
 st.plotly_chart(fig,use_container_width=True)
 
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-chat = st.chat_input('Informe sua duvidas')
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+for message in st.session_state.messages:
+     with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+if prompt := st.chat_input("E aí, posso te ajudar?"):
+    st.session_state.messages.apend({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    full_response = ""
+    for response in openai.ChatCompletion.create(
+        model=st.session_state["openai_model"],
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream='True',
+     ):
+        full_response += response.choices[0].delta.get("content","")
+        message_placeholder.markdown(full_response + "")
+    message_placeholder.markdown(full_response)
+st.session_state.messages.apend({"role": "assistant", "content": full_response})
